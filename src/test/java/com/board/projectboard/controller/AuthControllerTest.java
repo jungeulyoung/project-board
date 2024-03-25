30a12cde19db926cc6db432d915755e93eebe153
eulyoung jung <81278853+jungeulyoung@users.noreply.github.com>
2024-03-26 02:44:44 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동생성해준 '/login' 컨트롤러 테스트
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 '@Import' 추가

---
+++ src/test/java/com/board/projectboard/controller/AuthControllerTest.java
@@ -0,0 +1,39 @@
+package com.board.projectboard.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.board.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DisplayName("View 컨트롤러 - 인증")
+@Import(SecurityConfig.class)
+@WebMvcTest
+public class AuthControllerTest {
+    private final MockMvc mvc;
+
+    AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenTryToLogIn_thenReturnLogInView() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+}
